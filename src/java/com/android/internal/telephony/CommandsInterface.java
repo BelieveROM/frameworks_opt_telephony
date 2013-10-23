1c149b9448a3a41de1464016493ef920c931102e
jakew02 <jacob.whatley91@gmail.com>
2013-10-23 13:13:09 -0400

    fw_opt_telephony: Fix RIL for all Samsung devices
    By SferaDev and jakew02
    
    Add Samsung RIL files from CM
    
    Fix dependencies and errors at RIL.java
    
    Create Operators.java
    
    RIL: Add compatibility with qcom's DSDS:
    Devices with radio libraries built against a DSDS-enabled qualcomm
    BSP (anything in the last 4 or 5 months, apparently) have a slight
    incompatibility in the RIL protocol: it expects the first 4 bytes
    of the datastream to be the slot's identification (even if the
    device isn't dual-SIM). Absence of those 4 bytes will make the
    connection hang.
    
    PS2: Rebased on top of dependencies.
    PS3: Fixed whitespaces.
    PS4: Added radio states used by vendor rils.
    
    Change-Id: Ie5894e4cf639b57b0e44bf63d6abb414448e9da0
    
    Conflicts:
    	src/java/com/android/internal/telephony/RIL.java

--- src/java/com/android/internal/telephony/CommandsInterface.java
+++ src/java/com/android/internal/telephony/CommandsInterface.java
@@ -1623,6 +1623,11 @@ public interface CommandsInterface {
      */
     public void testingEmergencyCall();
 
+    /**
+     * @hide
+     * CM-specific: Ask the RIL about the presence of back-compat flags
+     */
+     public boolean needsOldRilFeature(String feature);
 
     /**
      * @return version of the ril.
